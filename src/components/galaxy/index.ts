d7475b76c998cf52c7190d1354da5a8ae3cf2c61
aftongauntlett <afton.gauntlett@gmail.com>
2025-06-23 14:18:39 -0400

    feat: organize themed components into folders and add barrel files for cleaner imports

---
+++ src/components/galaxy/index.ts
@@ -0,0 +1,5 @@
+export { default as GalaxyScene } from './GalaxyScene';
+export { default as LogicTerminal } from './LogicTerminal';
+export { default as MissionContent } from './MissionContent';
+export { default as ShipModel } from './ShipModel';
+export { default as TerminalPanel } from './TerminalPanel';
