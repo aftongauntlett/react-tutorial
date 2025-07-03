3b8d81216dee33fc30df0298479ef38f259d4257
aftongauntlett <afton.gauntlett@gmail.com>
2025-07-03 01:34:05 -0400

    Cleaning codebase, refactor story

--- src/components/galaxy/index.ts
+++
@@ -1,4 +0,0 @@
-export { default as GalaxyScene } from './GalaxyScene';
-export { default as LogicTerminal } from './LogicTerminal';
-export { default as MissionContent } from './MissionContent';
-export { default as ShipModel } from './ShipModel';
